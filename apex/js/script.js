c7d3d912e58bbad7b4404aec9a6633e4b77d469b
F1reMonster <62647213+F1reMonster@users.noreply.github.com>
2020-06-30 18:18:06 +0300

    adaptive

--- apex/js/script.js
+++ apex/js/script.js
@@ -17,10 +17,27 @@ function testWebP(callback) {
 $('.clients__slider').slick({
     prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="img/arrow-left.svg"></button>',
     nextArrow: '<button type="button" class="slick-btn slick-next"><img src="img/arrow-right.svg"></button>',
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        // autoplay: true,
-        // autoplaySpeed: 2000,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                infinite: true,
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+            }
+            
+    ]
 });
 
 
