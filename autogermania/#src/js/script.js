eb7197adabc43d1de0447ef9c725ce0731fdd406
F1reMonster <62647213+F1reMonster@users.noreply.github.com>
2020-11-06 01:29:32 +0200

    search.html final v1

--- autogermania/#src/js/script.js
+++ autogermania/#src/js/script.js
@@ -62,7 +62,6 @@ jQuery(function ($) {
 
 		});
 
-
 		bestOfferSlider.slick({
 			infinite: true,
 			slidesToShow: 4,
@@ -99,6 +98,47 @@ jQuery(function ($) {
 
 			]
 		});
+
+
+		var numSlick = 0;
+		var carCardSlider = $('.car-item__img-container');
+		//var paginInfo = $('.paging-info');
+
+		numSlick = 0;
+		carCardSlider.each(function () {
+
+			numSlick++;
+
+			var newSlider = $(this).addClass('slider-' + numSlick);
+
+			var status = newSlider.next('.paging-info').addClass('page-' + numSlick);
+
+			newSlider.on('init reInit afterChange', function (event, slick, currentSlide, nextSlide) {
+				//currentSlide is undefined on init -- set it to 0 in this case (currentSlide is 0 based)
+				var i = (currentSlide ? currentSlide : 0) + 1;
+				status.text(i + ' | ' + slick.slideCount);
+			})
+
+
+			newSlider.slick({
+				infinite: true,
+				slidesToShow: 1,
+				slidesToScroll: 1,
+				prevArrow: '<button type="button" class="carslider-btn prev"></button>',
+				nextArrow: '<button type="button" class="carslider-btn next"></button>',
+				
+			});
+
+		});
+
+
+		// var $status = $('.paging-info');
+
+
+
+
+
+
 
 		// кнопка "на гору" у футері
 		btnFooterUp.on('click', function (e) {
@@ -254,14 +294,14 @@ jQuery(function ($) {
 	// * ============ rangeSlider ============== * //
 
 	const filterYear = document.querySelector('.filter-year'),
-			filterKm = document.querySelector('.filter-km'),
-			filterPower = document.querySelector('.filter-power');
+		filterKm = document.querySelector('.filter-km'),
+		filterPower = document.querySelector('.filter-power');
 
 	noUiSlider.create(filterYear, {
 		start: [1990, 2020],
 		connect: true,
 		step: 1,
-		tooltips: [wNumb({decimals: 0}),wNumb({decimals: 0})],
+		tooltips: [wNumb({ decimals: 0 }), wNumb({ decimals: 0 })],
 		range: {
 			'min': 1990,
 			'max': 2020
@@ -272,7 +312,7 @@ jQuery(function ($) {
 		start: [0, 300000],
 		connect: true,
 		step: 10000,
-		tooltips: [wNumb({decimals: 0}),wNumb({decimals: 0})],
+		tooltips: [wNumb({ decimals: 0 }), wNumb({ decimals: 0 })],
 		range: {
 			'min': 0,
 			'max': 300000
@@ -283,7 +323,7 @@ jQuery(function ($) {
 		start: [50, 1000],
 		connect: true,
 		step: 10,
-		tooltips: [wNumb({decimals: 0}),wNumb({decimals: 0})],
+		tooltips: [wNumb({ decimals: 0 }), wNumb({ decimals: 0 })],
 		range: {
 			'min': 50,
 			'max': 1000
