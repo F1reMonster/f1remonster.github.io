454b350fc242de4a7f591d66e3764e40e2ebdd7b
F1reMonster <62647213+F1reMonster@users.noreply.github.com>
2020-11-08 11:52:42 +0200

    edit
    отключив webp

--- autogermania/dist/js/script.js
+++ autogermania/dist/js/script.js
@@ -1,18 +1,18 @@
-function testWebP(callback) {
+// function testWebP(callback) {
 
-	var webP = new Image();
-	webP.onload = webP.onerror = function () {
-		callback(webP.height == 2);
-	};
-	webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
-}
+// 	var webP = new Image();
+// 	webP.onload = webP.onerror = function () {
+// 		callback(webP.height == 2);
+// 	};
+// 	webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+// }
 
-testWebP(function (support) {
+// testWebP(function (support) {
 
-	if (support == true) {
-		document.querySelector('body').classList.add('webp');
-	}
-});
+// 	if (support == true) {
+// 		document.querySelector('body').classList.add('webp');
+// 	}
+// });
 
 
 jQuery(function ($) {
